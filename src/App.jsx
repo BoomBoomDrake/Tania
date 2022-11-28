9def42f97295b72cef4280e031ae82ab01313cdd
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-27 19:03:49 -0600

    Generated and imported Dialogue and DisplayWindow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import DisplayWindow from "./components/DisplayWindow";
+import Dialogue from "./components/Dialogue";
 
 export default class App extends Component {
   render() {
-    return <h1>Test</h1>;
+    return (
+      <div id="app-container" className="container-md border text-center mt-5">
+        <DisplayWindow />
+        <Dialogue />
+      </div>
+    );
   }
 }
---
+++ src/components/DisplayWindow.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class DisplayWindow extends Component {
+  render() {
+    return <h1>DisplayWindow</h1>;
+  }
+}
