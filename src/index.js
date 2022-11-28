94f8d826defd18c4a45d64b6aef7b88b0a1ee91f
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-27 18:47:43 -0600

    Linked index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "popper.js/dist/umd/popper";
+import "@popperjs/core";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.jsx";
