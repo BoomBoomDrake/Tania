40bf64912ffb3a5e4557f8871bf576005d0b659d
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-28 19:45:07 -0600

    Generated text and buttons to display based on scene

--- src/components/Dialogue.jsx
+++ src/components/Dialogue.jsx
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import { storyArc } from "../scenes";
 
 export default class Dialogue extends Component {
+  state = {
+    scene: storyArc,
+  };
+
   render() {
-    return <h1>Dialogue</h1>;
+    return (
+      <React.Fragment>
+        <h1>{this.state.scene.name}</h1>
+        <p>{this.state.scene.text}</p>
+        <div>
+          {this.state.scene.choices.map((choice) => {
+            return (
+              <button
+                key={choice}
+                data-path={choice}
+                onClick={this.sceneChange}
+              >
+                {choice.name}
+              </button>
+            );
+          })}
+        </div>
+      </React.Fragment>
+    );
   }
+
+  sceneChange = (event) => {
+    // this.setState({ scene: event.target.dataset.path });
+    console.log(event.target.dataset.path.name);
+    console.log("button clicked");
+  };
 }
